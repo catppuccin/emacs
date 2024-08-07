10790024ba6f34fdc37d5f47eed11f5758a68e9d
Jeremy Baxter <jeremy@baxters.nz>
2024-08-08 11:33:08 +1200

    ci: run .el files against byte-compile-file

--- scripts/check-lisp-files.el
+++ scripts/check-lisp-files.el
@@ -1,5 +1,6 @@
 ;;; indent-lisp-files.el --- call `indent-region' over all Lisp files -*- lexical-binding: t; -*-
 
+(require 'bytecomp)
 (require 'seq)
 (when (version< emacs-version "29.1")
   (require 'compat)) ; file-name-parent-directory
@@ -17,9 +18,13 @@
     (files (.el-files-in root-path))
     (scripts (.el-files-in (concat root-path "/scripts/"))))
   (dolist (file (append files scripts))
+    (setq
+      byte-compile-error-on-warn t
+      indent-tabs-mode nil
+      lisp-indent-offset 2)
+    (unless (byte-compile-file file)
+      (error "byte-compile-file failed (%s)" file))
     (with-current-buffer (find-file-noselect file)
-      (setq indent-tabs-mode nil
-        lisp-indent-offset 2)
       (indent-region (point-min) (point-max))
       (save-buffer)
       (kill-buffer (current-buffer)))))
