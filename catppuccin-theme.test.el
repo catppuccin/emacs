bbd2a35861ab6b5fdfc15c8953d817c5f85cb551
Jeremy Baxter <jeremy@baxters.nz>
2024-08-13 20:12:28 +1200

    chore: fix style in tests

--- catppuccin-theme.test.el
+++ catppuccin-theme.test.el
@@ -1,8 +1,25 @@
-;;; catppuccin-theme.test.el --- Tests for The Catppuccin Emacs Theme -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; catppuccin-theme.test.el --- tests for the Catppuccin theme -*- lexical-binding: t; no-byte-compile: t; -*-
 
-;; Copyright 2022-present, All rights reserved
+;; Copyright 2022-present Catppuccin, All rights reserved
 ;;
-;; Code licensed under the MIT license
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included
+;; in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ;; Maintainer: Carsten Kragelund <carsten@kragelund.me>
 ;; Author: nyxkrage
@@ -15,14 +32,13 @@
 ;; 🍄 Soothing pastel tests for The Catppuccin Emacs Theme
 
 ;;; Code:
-(defmacro assert (test-form message)
-  `(when (not ,test-form)
-     (error "Assertion failed: %s\n%s"
-       (format "%s" ',test-form) ,message)))
+(defmacro assert (form message)
+  `(unless ,form
+     (error "Assertion failed: %s\n%s" (format "%s" ',form) ,message)))
 (advice-add 'display-color-cells :override (lambda (&rest r) 16777216))
 
 (add-to-list 'custom-theme-load-path (file-name-directory load-file-name))
-(load-theme 'catppuccin t) ;; Load theme functions
+(load-theme 'catppuccin t) ; load theme functions
 (setq catppuccin-flavor 'mocha)
 (catppuccin-set-color 'base "#000000")
 (catppuccin-reload)
@@ -36,4 +52,4 @@
 (assert (string-equal "#000000" (catppuccin-color 'base))
   "Base does not match custom specified after switching back")
 
-;;; catppuccin-tests.test..el ends here
+;;; catppuccin-theme.test.el ends here
